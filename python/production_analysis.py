5d331b18ba7b461dbeaf6fcb4b4750869246f792
Abdulla A. B. <babulwriting@gmail.com>
2026-04-11 06:01:22 +0600

    Update production_analysis.py

--- python/production_analysis.py
+++ python/production_analysis.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+df = pd.read_csv("../data/production.csv")
+
+df["Gap"] = df["Target"] - df["Actual"]
+df["Efficiency"] = (df["Actual"] / df["Target"]) * 100
+
+problem = df.loc[df["Gap"].idxmax()]
+
+print("=== DATA DRIVEN DECISION ===")
+print("Problem Section:", problem["Section"])
+print("Efficiency:", round(problem["Efficiency"],2))
+
+print("\nFull Analysis")
+print(df)
