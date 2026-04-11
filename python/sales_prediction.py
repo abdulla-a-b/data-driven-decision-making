32f6cc8cc668396b5482c0b3f432be89faf6e735
Abdulla A. B. <babulwriting@gmail.com>
2026-04-11 06:01:45 +0600

    Update sales_prediction.py

--- python/sales_prediction.py
+++ python/sales_prediction.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
+df = pd.read_csv("../data/sales.csv")
+
+df["MonthNo"] = np.arange(len(df))
+
+X = df[["MonthNo"]]
+y = df["Sales"]
+
+model = LinearRegression()
+model.fit(X,y)
+
+future = [[12]]
+prediction = model.predict(future)
+
+print("Next Month Sales Prediction:", int(prediction[0]))
