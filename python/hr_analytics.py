f271e6f7eaef74ec3a912e5fd50a98b9089f9c57
Abdulla A. B. <babulwriting@gmail.com>
2026-04-11 06:02:03 +0600

    Update hr_analytics.py

--- python/hr_analytics.py
+++ python/hr_analytics.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+data = {
+"Dept":["Production","Sales","HR","Accounts"],
+"Employees":[200,120,40,30],
+"Resigned":[30,25,2,1]
+}
+
+df = pd.DataFrame(data)
+
+df["Attrition"] = (df["Resigned"]/df["Employees"])*100
+
+print(df)
